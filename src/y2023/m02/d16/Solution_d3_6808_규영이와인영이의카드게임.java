e64214a90ba267b06932364584150662084bc780
dnjsndnjs <37894963+dnjsndnjs@users.noreply.github.com>
2023-02-16 22:41:42 +0900

    0216

--- src/y2023/m02/d16/Solution_d3_6808_규영이와인영이의카드게임.java
+++ src/y2023/m02/d16/Solution_d3_6808_규영이와인영이의카드게임.java
@@ -15,33 +15,38 @@ public class Solution_d3_6808_규영이와인영이의카드게임 {
 		
 		int TC = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <=TC; tc++) {
-			int[] arr = new int[9];
+			int[] A = new int[9];
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			for (int i = 0; i < 9; i++)
-				arr[i] = Integer.parseInt(st.nextToken());
-			Arrays.sort(arr);
+				A[i] = Integer.parseInt(st.nextToken());
+			Arrays.sort(A);
+			int[] B = new int[9];
+			int idx = 0;
+			for (int i = 1; i <= 18; i++) {
+				if (Arrays.binarySearch(A, i) < 0) B[idx++] = i;
+			}
 			win = 0; lose = 0;
-			perm(0, arr, 0, new boolean[19]);
+			perm(0, A, B, 0, new boolean[9]);
 			sb.append("#"+tc+" "+win+" "+lose+"\n");
 		}
 		System.out.print(sb.toString());
 		br.close();
 	}
 	
-	static void perm(int cnt, int[] arr, int score, boolean[] v) {
+	static void perm(int cnt, int[] A, int[] B, int score, boolean[] v) {
 		if (cnt == 9) {
 			if (score > total/2)
-				win++;
-			else lose++;
+				lose++;
+			else win++;
 			return;
 		}
-		for (int i = 1; i <= 18; i++) {
-			if (Arrays.binarySearch(arr, i) >= 0) continue;
+		for (int i = 0; i < 9; i++) {
 			if (v[i]) continue;
 			v[i] = true;
-			if (i > arr[cnt])
-				score += arr[cnt] + i;
-			perm(cnt+1, arr, score, v);
+			if (B[i] > A[cnt])
+				perm(cnt+1, A, B, score + A[cnt] + B[i], v);
+			else
+				perm(cnt+1, A, B, score, v);
 			v[i] = false;
 		}
 	}
