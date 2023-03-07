fd3917eb368d14f8fe2a18564423b9a68506b977
dnjsndnjs <37894963+dnjsndnjs@users.noreply.github.com>
2023-03-07 22:09:07 +0900

    0307

--- src/y2023/m03/d07/Main_bj_21611_마법사상어와블리자드_서울_20반_임성원.java
+++ src/y2023/m03/d07/Main_bj_21611_마법사상어와블리자드_서울_20반_임성원.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class Main_bj_21611_마법사상어와블리자드_서울_20반_임성원 {
 	static final int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
-	static final int[] coeff = {3, -1, -3, 1};
+	static final int[] dd = {3, 1, 0, 2};
+	
+	static int[][] del;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,55 +20,7 @@ public class Main_bj_21611_마법사상어와블리자드_서울_20반_임성원
 			for (int j = 0; j < N; j++)
 				map[i][j] = Integer.parseInt(st.nextToken());
 		}
-		List<Integer> ball = new ArrayList<>(N*N);
-		int d = 0, x = N/2, y = N/2;
-		ball.add(0);
-		l:for (int l = 1; l <= N; l++) {
-			for (int k = 0; k < 2; k++) {
-				for (int i = 0; i < l; i++) {
-					if (x == 0 && y == 0) break l;
-					x+=dx[d]; y+=dy[d];
-					if (map[x][y] == 0) break l;
-					ball.add(map[x][y]);
-				}
-				if (++d == 4) d = 0;
-			}
-		}
 		int[] exp = new int[4];
-		for (int k = 0; k < M; k++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			d = Integer.parseInt(st.nextToken())-1;
-			for (int s = Integer.parseInt(st.nextToken()); s > 0; s--) {
-				int idx = (4*s+coeff[d])*s;
-				if (idx >= ball.size()) continue;
-				ball.remove(idx);
-			}
-			List<Integer> next = new ArrayList<>();
-			next.add(0);
-			int cnt = 1, prev = ball.get(1);
-			for (int i = 2, size = ball.size(); i < size; i++) {
-				int cur = ball.get(i);
-				if (cur == prev) {
-					cnt++;
-				} else if (cnt >= 4) {
-					exp[prev] += cnt;
-					if (next.size() == 1) {
-						prev = cur;
-						cnt = 1;
-						continue;
-					}
-					i--;
-					prev = next.remove(next.size()-1);
-					cnt = next.remove(next.size()-1);
-				} else {
-					next.add(cnt);
-					next.add(prev);
-					prev = cur;
-					cnt = 1;
-				}
-			}
-			ball = next;
-		}
 		int ans = 0;
 		for (int i = 1; i < 4; i++) {
 			ans += i*exp[i];
